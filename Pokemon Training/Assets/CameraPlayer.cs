acf565d81f50a198769737753477539e4a6eac1b
Irene <delbarco94@hotmail.com>
2015-05-16 15:22:46 +0200

    Added PlayerMovement and Camera Script.

---
+++ Pokemon Training/Assets/CameraPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayer : MonoBehaviour {
+
+	public Transform player;
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.orthographicSize = (Screen.height / 100f) / 4f;
+
+		if (player) {
+			transform.position = player.position + new Vector3(0, 0, -10);
+		}
+	}
+}
---
+++ Pokemon Training/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movement != Vector2.zero) {
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat("input_x", movement.x);
+			anim.SetFloat("input_y", movement.y);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+
+		rb.MovePosition (rb.position + movement * Time.deltaTime);
+	}
+}
