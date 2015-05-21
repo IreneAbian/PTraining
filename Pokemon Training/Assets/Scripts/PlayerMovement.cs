ccf4e7abe54dd41187e6362216bb7b3e03eb34cd
Irene <delbarco94@hotmail.com>
2015-05-21 22:35:43 +0200

    Control (incompleto) del movimiento en Android.

--- Pokemon Training/Assets/Scripts/PlayerMovement.cs
+++ Pokemon Training/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D rb;
 	Animator anim;
+	private Vector2 touchOrigin = -Vector2.one;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +15,36 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 movement;
-		#if UNITY_STANDALONE || UNITY_WEBPLAYER
-		movement = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 movement = Vector2.zero;
+
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
+
+        movement = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 
 		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-		Debug.Log("Soy un comentario. Si, sin "//". Y punto.");
+
+		if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).position.x > Screen.width / 2)
+            {
+                movement.x = 1;
+            } else if (Input.GetTouch(0).position.x < Screen.width / 2)
+            {
+                movement.x = -1;
+            }
+
+            if (Input.GetTouch(0).position.y > Screen.width / 2)
+            {
+                movement.y = 1;
+            } 
+            else if (Input.GetTouch(0).position.y < Screen.width / 2)
+            {
+                movement.y = -1;
+            }
+
+
+        }
+
 		#endif
 
 		if (movement != Vector2.zero) {
