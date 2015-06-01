3be41d81a359ad2f199478c2e4a617a69cf5b88d
Irene <delbarco94@hotmail.com>
2015-06-02 00:01:35 +0200

    Mejorados los métodos para las imágenes

--- Pokemon Training/Assets/Scripts/Database/DAO/PokemonBasicDAO.cs
+++ Pokemon Training/Assets/Scripts/Database/DAO/PokemonBasicDAO.cs
@@ -1,5 +1,12 @@
 using SQLite4Unity3d;
 using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System;
 
 public class PokemonBasicDAO{
 
@@ -15,4 +22,12 @@ public class PokemonBasicDAO{
 		return DataService.instance._connection.Table<PokemonBasic> ().Where (x => x.Type == type);
 	}
 
+	public void InsertPokemon(int id, byte[] sprite){
+		DataService.instance._connection.Execute ("Insert into PokemonBasic (Id, SpriteImage) values (" + id + ", '" + Encoding.Default.GetString(sprite) + "')");
+	}
+
+	public byte[] GetPokemonSprite(int id){
+		PokemonBasic pkm = DataService.instance._connection.Table<PokemonBasic> ().Where (x => x.Id == id).First ();
+		return System.Text.Encoding.UTF8.GetBytes(pkm.SpriteImage);
+	}
 }
