8c7ca7a7fbe67dfdd673f93dc0911e5177257df6
Irene <delbarco94@hotmail.com>
2015-06-10 00:11:09 +0200

    Continúa interfaz de equipo

--- Pokemon Training/Assets/MostrarDatosPrimerPokemon.cs
+++ Pokemon Training/Assets/MostrarDatosPrimerPokemon.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class MostrarDatosPrimerPokemon : MonoBehaviour {
 
+	public GameObject nombre;
+	public GameObject nivel;
+	public GameObject descripcion;
+	public GameObject datos;
+	public Sprite sprite;
+
 	void Start () {
-	
+		
 	}
+
 
 }
